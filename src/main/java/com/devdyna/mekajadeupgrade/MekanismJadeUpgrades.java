e898a0a241401162f2231e5b5771ea59e5df60f2
mochi-753 <179483962+mochi-753@users.noreply.github.com>
2026-02-24 23:05:35 +0900

    Modのメインクラス修正

---
+++ src/main/java/com/devdyna/mekajadeupgrade/MekanismJadeUpgrades.java
@@ -0,0 +1,20 @@
+package com.devdyna.mekajadeupgrade;
+
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(MekanismJadeUpgrades.MOD_ID)
+public class MekanismJadeUpgrades {
+
+    public static final String MOD_ID = "mekajadeupgrade";
+
+    @SuppressWarnings("removal")
+    public MekanismJadeUpgrades() {
+        this(FMLJavaModLoadingContext.get());
+    }
+
+    public MekanismJadeUpgrades(FMLJavaModLoadingContext context) {
+
+    }
+}
